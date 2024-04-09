42957e55d8befcc91caa1676ac36e9e1aeae6fcc
tkykkkkk <158381693+tkykkkkk@users.noreply.github.com>
2024-04-09 13:03:55 +0000

    adding searche done

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,4 +35,18 @@ class User < ApplicationRecord
    following_users.include?(user)
  end 
  
+ def self.looks(search, word)
+   if search == "perfect_match"
+     @user = User.where("name LIKE?", "#{word}")
+   elsif search == "forward_match"
+     @user = User.where("name LIKE?", "#{word}%")
+   elsif search == "backward_match"
+     @user = User.where("name LIKE?", "%#{word}")
+   elsif search == "partial_match"
+     @user = User.where("name LIKE?", "%#{word}%")
+   else
+     @user = User.all
+   end 
+ end 
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get "/home/about" => "homes#about", as: "about"
+  get "search" => "searches#search"
   devise_for :users
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do 
     resources :book_comments, only: [:create, :destroy]
@@ -12,5 +13,7 @@ Rails.application.routes.draw do
       end 
       resource :relationships, only: [:create, :destroy]
   end 
+  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
